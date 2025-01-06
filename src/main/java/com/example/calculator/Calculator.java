3ed6e1a30bb25bba57ddf0e2a6ca13e4a5a3b302
minkyungCHO <mk981015@naver.com>
2025-01-06 19:15:54 +0900

    feat: 양의 정수 2개(0 포함)를 입력 받기

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        Scanner sc = new Scanner(System.in);
+
+        // 양의 정수 2개(0 포함)를 입력 받기
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNum = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNum = sc.nextInt();
     }
 }
