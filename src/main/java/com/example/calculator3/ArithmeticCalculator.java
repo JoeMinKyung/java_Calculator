a4cfe1570e709a3df7e56e4833f6f2f46fd6761f
minkyungCHO <mk981015@naver.com>
2025-01-16 13:08:00 +0900

    feat: List 변수명을 results->resultList로 변경, List 타입을 제너릭으로 선언, 제너릭 타입 정보를 받을 Class<T> type 변수 선언, 생성자를 추가하여 타입 정보를 갱신

--- src/main/java/com/example/calculator3/ArithmeticCalculator.java
+++ src/main/java/com/example/calculator3/ArithmeticCalculator.java
@@ -6,7 +6,13 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ArithmeticCalculator<T extends Number> {
-    private final List<Double> results = new ArrayList<>();
+    private final List<T> resultList = new ArrayList<>();
+    private final Class<T> type;
+
+    // 생성자
+    public ArithmeticCalculator(Class<T> type) {
+        this.type = type;
+    }
 
     // 연산 수행
     public <T extends Number> double calculate(T a, T b, Calculator3.OperatorType operatorType) {
@@ -17,26 +23,26 @@ public class ArithmeticCalculator<T extends Number> {
 
     // 연산 결과 추가
     private void addResult(double result) {
-        results.add(result);
+        resultList.add(result);
     }
 
     // 연산 결과를 가져오기
     public List<Double> getResults() {
-        return new ArrayList<>(results);
+        return new ArrayList<>(resultList);
     }
 
     // 연산 결과 기록 출력
     public void printResults() {
         System.out.println("=========== 연산 기록 ===========");
-        for (int i = 0; i < results.size(); i++) {
-            System.out.println((i + 1) + ". " + results.get(i));
+        for (int i = 0; i < resultList.size(); i++) {
+            System.out.println((i + 1) + ". " + resultList.get(i));
         }
     }
 
     // 가장 먼저 저장된 연산 결과 삭제
     public void removeResult() {
-        if (!results.isEmpty()) {
-            results.remove(0);
+        if (!resultList.isEmpty()) {
+            resultList.remove(0);
         } else {
             System.out.println("삭제할 연산 결과가 없습니다.");
         }
@@ -44,7 +50,7 @@ public class ArithmeticCalculator<T extends Number> {
 
     // 주어진 값보다 큰 연산 결과들 출력 (람다 & 스트림 사용)
     public void printResultsGreaterThan(double value) {
-        List<Double> greaterResults = results.stream()
+        List<Double> greaterResults = resultList.stream()
                 .filter(result -> result > value)
                 .collect(Collectors.toList());
 
